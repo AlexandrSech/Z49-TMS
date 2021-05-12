9cf6408c7d507d98b1a814db9c68fcb56ea060e8
Валентина <valentina@MacBook-Pro-Valentina.local>
2021-05-12 16:58:54 +0300

    Tasks 3 by Ivanova

--- students/Ivanova/tasks_3/venv/bin/activate.fish
+++ students/Ivanova/tasks_3/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/valentina/Desktop/tasks/venv"
+set -gx VIRTUAL_ENV "/Users/valentina/Desktop/pythonProject1/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

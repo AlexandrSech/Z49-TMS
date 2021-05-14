f219aab4753e1b72245fca814aa7de8760f7cf63
FilCherry <wolf.fil94@gmail.com>
2021-05-14 21:01:12 +0300

    hw_l2

---
+++ students/Cherednichenko/HW/Test_1.py
@@ -0,0 +1,21 @@
+#1
+print(2+3, 2-3, 2*3, sep='\n')
+print()
+#2
+x=-5
+y=3
+print((abs(x)-abs(y))/(1+abs(x*y)))
+print()
+#3
+z=6
+print(z ** 3, z ** 2, sep='\n')
+print()
+#4
+a1=3
+a2=6
+print((a1+a2)/2, (a1*a2)**0.5, sep='\n')
+print()
+#5
+b1,b2 = 4,5
+print((b1**2+b2**2)**0.5)
+print(0.5*b1*b2)

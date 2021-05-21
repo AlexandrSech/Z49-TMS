1c2c969be3935d42069b3887df725f76aceffe16
root <artyomtitov99@mail.ru>
2021-05-21 17:40:16 +0300

    21.05.21

--- students/Titov/course_4.py
+++ students/Titov/course_4.py
@@ -1,3 +1,29 @@
+import random
+spis = []
+m = 0
+n = 3
+for i in range(4):
+    spis.append([])
+    for j in range(4):
+
+        if i == j:
+            spis[i].append(1)
+        elif i == m and j == n:
+            spis[i].append(1)
+        else:
+            spis[i].append(0)
+    m += 1
+    n -= 1
+    print(spis[i])
+for i in range(len(spis)):
+    for j in range(len(spis[i])):
+        print(0)
+
+
+
+
+
+exit()
 for i in range(200, 301): #220
     sum_of_divide = 0
     n = 0
--- students/Titov/task_5_2.py
+++ students/Titov/task_5_2.py
@@ -1,3 +1,4 @@
+'''Дано число. Найти сумму и произведение его цифр.'''
 chislo = input('Введите число')
 new_chislo = 0
 for el in range(len(chislo)):

925000d53f50f381a913a7716eebe6845e2ea920
ilijahNod <kersankaifat11@gmail.com>
2021-05-28 06:53:56 +0300

    L7

--- students/Hantsevich/L3/task_3_4.py
+++ students/Hantsevich/L3/task_3_4.py
@@ -1,6 +1,8 @@
 word = input()
 l = len(word)
 mid = l // 2
-print(word[(mid-1)])
-if word[(mid-1)] == word[0]:
-    print(word[1:(l - 1)])
+
+if word[(mid)] == word[0]:
+    print(word[:(l)])
+else:
+    print(word[(mid-1)])

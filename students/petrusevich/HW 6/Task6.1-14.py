bd0e798fb574e54a8e5750b22f67c81c59e4314d
galetttt <petrusevich.g64@gmail.com>
2021-06-09 18:50:20 +0300

    пункт3

--- students/petrusevich/HW 6/Task6.1-14.py
+++ students/petrusevich/HW 6/Task6.1-14.py
@@ -12,8 +12,12 @@ for i in range(n):
 
 
 max_el = 0
+min_el = b
 for item, value in enumerate(matrix):
     for i, j in enumerate(value):
         if j > max_el:
             max_el = j
+        if j < min_el:
+            min_el = j
 print('Максимальный элемент матрицы:', max_el)
+print('Минимальный элемент матрицы:', min_el)
